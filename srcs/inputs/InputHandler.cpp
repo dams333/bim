96a98c1820bf2a72ae1e74128cec74e8915af526
Damien Hubleur <damien.hubleur@coderpad.io>
2024-04-26 14:37:47 +0200

    feat(inputs): handle backspace to delete

--- srcs/inputs/InputHandler.cpp
+++ srcs/inputs/InputHandler.cpp
@@ -48,6 +48,16 @@ bool Editor::inputHandler(int input) {
 				this->cursor.setPos(0);
 				this->repairCursor();
 			} else if (input == 127) { // Backspace
+				if (this->cursor.getPos() > 0) {
+					this->contentBuffer.setLine(this->cursor.getLine(), this->contentBuffer.getLine(this->cursor.getLine()).substr(0, this->cursor.getPos() - 1) + this->contentBuffer.getLine(this->cursor.getLine()).substr(this->cursor.getPos()));
+					this->cursor.setPos(this->cursor.getPos() - 1);
+				} else if (this->cursor.getLine() > 0) {
+					int lineLength = this->contentBuffer.getLine(this->cursor.getLine() - 1).length();
+					this->contentBuffer.setLine(this->cursor.getLine() - 1, this->contentBuffer.getLine(this->cursor.getLine() - 1) + this->contentBuffer.getLine(this->cursor.getLine()));
+					this->contentBuffer.removeLine(this->cursor.getLine());
+					this->cursor.setLine(this->cursor.getLine() - 1);
+					this->cursor.setPos(lineLength);
+				}
 			} else { // char
 				this->contentBuffer.append(this->cursor.getLine(), this->cursor.getPos(), input);
 				this->cursor.setPos(this->cursor.getPos() + 1);
